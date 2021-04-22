921aaba64313327eabc9ae4eae1b15454d2a114e
MorelAtala <75019641+MorelAtala@users.noreply.github.com>
2021-04-22 16:40:16 +0200

    Update settings.py

--- kannweg/kannweg/settings.py
+++ kannweg/kannweg/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c(fnaqi3&q_xd22#h4@efbn56csi8=q_**j(wvh$34)qdb=5mi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kusomapp.herokuapp.com']
 
 
 # Application definition
