a455774f3c7fe0ac9bae78627483168b512ef4b4
MorelAtala <75019641+MorelAtala@users.noreply.github.com>
2021-04-26 15:08:12 +0200

    Update models.py

--- kannweg/todo/models.py
+++ kannweg/todo/models.py
@@ -27,7 +27,6 @@ class Product(models.Model):
 	name = models.CharField(max_length=200, null=True)
 	price = models.FloatField(null=True)
 	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-	description = models.CharField(max_length=200, null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	tags = models.ManyToManyField(Tag)
 
@@ -45,7 +44,7 @@ class Order(models.Model):
 	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
-	description = models.CharField(max_length=100, null=True)
+	description = models.CharField(max_length=200, null=True)
 	def __str__(self):
 		return self.product.name
 
